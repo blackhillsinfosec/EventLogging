ff5eca3c13a8e0f50f4e22aa2bab5a2847a8bf32
0BL1V10N_55 <114760354+0BL1V10N-55@users.noreply.github.com>
2022-11-18 08:49:37 -0700

    Update and rename Update_WECs.ps1 to Update_WECs_v1.ps1

--- DEFCON4/Update_WECs_v1.ps1
+++ DEFCON4/Update_WECs_v1.ps1
@@ -15,10 +15,12 @@ cd "c:\temp"
 wecutil es > c:\temp\subscriptions.txt
 foreach($line in Get-Content C:\temp\subscriptions.txt) {wecutil ss /e:false $line}
 foreach($line in Get-Content C:\temp\subscriptions.txt) {wecutil ds $line}
+remove-item -path "c:\temp\EventLogging\" -recurse 
 Invoke-WebRequest -URI https://github.com/blackhillsinfosec/EventLogging/archive/master.zip -OutFile "C:\temp\EventLogging.zip"
 Expand-Archive C:\temp\EventLogging.zip
 cd "c:\temp\EventLogging\EventLogging-master\DEFCON4\WEF-Subscriptions"
 foreach ($file in (Get-ChildItem *.xml)) {wecutil cs $file}
+cd "c:\temp"
 remove-item "C:\temp\subscriptions.txt"
 remove-item "c:\temp\EventLogging.zip"
 remove-item -path "c:\temp\EventLogging\" -recurse 
